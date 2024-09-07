7bfff680fbccd56dc600859afdec6abea67687ca
jebasz <jebastinjj123@gmail.com>
2024-09-07 15:24:35 +0530

    backend commit

---
+++ bikequick/src/main/java/com/bike_rental/bikequick/BikequickApplication.java
@@ -0,0 +1,13 @@
+package com.bike_rental.bikequick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikequickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikequickApplication.class, args);
+	}
+
+}
